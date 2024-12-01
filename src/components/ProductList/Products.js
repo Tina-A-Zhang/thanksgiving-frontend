c21c97733ad11b0831f34af4808820fc3386077c
Tina <tinazhang001120@gmail.com>
2024-12-01 12:41:04 +1100

    updated price

--- src/components/ProductList/Products.js
+++ src/components/ProductList/Products.js
@@ -5,8 +5,8 @@ export const products = [
       "What’s your favourite sport? Whether you like Soccer or Table tennis, it’s hard to play without the right gear. Send sports equipment to a poor student in Egypt.",
     callToAction: "Send sports equipment to a poor student in Egypt.",
     image: require("../../assets/ProductList/football.png"),
-    priceImage: require("../../assets/ProductList/price-30.png"),
-    price: 30,
+    priceImage: require("../../assets/ProductList/price-9.5.png"),
+    price: 9.5,
   },
   {
     id: 2,
@@ -14,8 +14,8 @@ export const products = [
       "Fun Fact: Did you know that people don’t put their gifts under the Christmas tree in Egypt? Instead, they give cash!",
     callToAction: "Give a poor kid in Egypt some Christmas Money",
     image: require("../../assets/ProductList/money.png"),
-    priceImage: require("../../assets/ProductList/price-15.png"),
-    price: 15,
+    priceImage: require("../../assets/ProductList/price-9.5.png"),
+    price: 9.5,
   },
   {
     id: 3,
@@ -41,8 +41,8 @@ export const products = [
       "How great does it feel when you wear new clothes? Looking good can help you feel good too!",
     callToAction: "Give a kid in Egypt new clothes for Christmas",
     image: require("../../assets/ProductList/clothes.png"),
-    priceImage: require("../../assets/ProductList/price-24.png"),
-    price: 24,
+    priceImage: require("../../assets/ProductList/price-15.png"),
+    price: 15,
   },
   {
     id: 6,
@@ -68,8 +68,8 @@ export const products = [
       "Can you imagine not being able to see the blackboard at school… That would make learning hard, wouldn’t it?",
     callToAction: "Give a kid in The Philippines glasses to help them see",
     image: require("../../assets/ProductList/glasses.png"),
-    priceImage: require("../../assets/ProductList/price-15.png"),
-    price: 15,
+    priceImage: require("../../assets/ProductList/price-9.5.png"),
+    price: 9.5,
   },
   {
     id: 9,
@@ -78,8 +78,8 @@ export const products = [
     callToAction:
       "Help a kid in The Philippines have the best school holidays ever!",
     image: require("../../assets/ProductList/icecream.png"),
-    priceImage: require("../../assets/ProductList/price-9.5.png"),
-    price: 9.5,
+    priceImage: require("../../assets/ProductList/price-5.png"),
+    price: 5,
   },
   {
     id: 10,
@@ -96,8 +96,8 @@ export const products = [
       "Who says learning can’t be exciting? Help kids learn with toys and tools that make learning fun!",
     callToAction: "Give educational equipment to kids in Indonesia",
     image: require("../../assets/ProductList/education.png"),
-    priceImage: require("../../assets/ProductList/price-33.png"),
-    price: 33,
+    priceImage: require("../../assets/ProductList/price-15.png"),
+    price: 15,
   },
   {
     id: 12,
@@ -114,7 +114,7 @@ export const products = [
       "What’s one of the best things about going back to school? Picking your new pencils and books and a bag! It’s so exciting isn’t it!",
     callToAction: "Give a kid in Nepal new school stuff for Christmas",
     image: require("../../assets/ProductList/stationary.png"),
-    priceImage: require("../../assets/ProductList/price-30.png"),
-    price: 30,
+    priceImage: require("../../assets/ProductList/price-15.png"),
+    price: 15,
   },
 ];
